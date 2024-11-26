f1d5a43a25ac5334b9f8691e828b0adff280abaf
ignesh11 <jebanignesh99@gmail.com>
2024-11-26 17:04:01 +0530

    Add files via upload
    first application

---
+++ My_Application/src/com/hello/Addition.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The Addtion of two number is:"+c);
+
+	}
+
+}
